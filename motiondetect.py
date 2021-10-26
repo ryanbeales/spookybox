056672bbf4635444ffbbea236226dcb59f0060d1
Ryan Beales <ryanbeales@gmail.com>
2021-10-26 17:20:39 +0100

    Update movement detection

--- motiondetect.py
+++ motiondetect.py
@@ -4,40 +4,39 @@ import logging
 import threading, queue
 import time
 
+import numpy as np
+
 logger = logging.getLogger(__name__)
 
 def capture_loop():
     logger.info('Opening Video Capture')
     cap = cv2.VideoCapture(0)
-
-    classifer_name = 'haarcascade_upperbody.xml'
-    logger.info('Loading cascade classifer: {classifer_name}')
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + classifer_name)
-
+    bg = cv2.createBackgroundSubtractorMOG2(history=500, varThreshold=25, detectShadows=False)
+    kernel = np.ones((20,20), np.uint8)
 
     try:    
         logger.info('Starting capture loop')
         while True:
           logger.debug('Read image from capture stream')
           _, img = cap.read()
-          logger.debug('Convert to grayscale')
-          gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
           
-          logger.debug('Detecting objects...')
-          objects = face_cascade.detectMultiScale(gray,
-                  scaleFactor=1.1,
-                  minNeighbors=5,
-                  minSize=(30,30),
-                  flags=cv2.CASCADE_SCALE_IMAGE)
-
+          fgmask = bg.apply(img)
+          fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel)
+          fgmask = cv2.medianBlur(fgmask, 5)
+          _, fgmask = cv2.threshold(fgmask, 196, 255, cv2.THRESH_BINARY)
+          contours, heirachy = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+          areas = [cv2.contourArea(c) for c in contours if cv2.contourArea(c) > 20000]
+ 
           image_width = img.shape[1]
 
-          logger.debug(f'Found {len(objects)} objects')
-          for o in objects:
-             # If something is found, it's a set of (x,y,w,h)
-             x = o[0]
-             logger.debug(f"Found an object: x = {x}, image_width = {image_width}")
-            
+          logger.debug(f'Found {len(areas)} movement areas')
+
+          if len(areas) >= 1:
+              max_index = np.argmax(areas)
+              cnt = contours[max_index]
+              x, y, w, h = cv2.boundingRect(cnt)
+              logger.debug(f"Center of largest movement{x + (w/2)} of size {image_width}")
+
     finally:
         logger.info('Closing Video Capture')
         cap.release()
