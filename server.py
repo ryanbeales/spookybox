2a7a39bf2dd9573e9447e269ecf69df354020142
Ryan Beales <ryanbeales@gmail.com>
2022-10-23 19:22:39 +0100

    Better aiming numbers saved

--- server.py
+++ server.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'spooooooky'
 socketio = SocketIO(app)
 
-# Set up our servos
-horizontalservo = Servo(23)
+# Set up our servos, offset by -20 to account for where it was installed
+horizontalservo = Servo(23, offset=-20)
 verticalservo = Servo(27)
 
 @socketio.on('faceposition')
 def handle_message(data):
-    print(f'face: x={data[0]} y={data[1]}')
+    print(f'face: x={data[0]} y={data[1]} maxx={data[2]} maxy={data[3]}')
 
     # Move eyes
-    horizontalservo.set_fraction(-data[0]/640)
-    verticalservo.set_fraction(data[1]/480)
+    horizontalservo.set_fraction((data[2]-data[0])/data[2], minimum=-45, maximum=30)
+    verticalservo.set_fraction(data[1]/data[3], minimum=-45, maximum=45)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0')
